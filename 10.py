b05bde3456e84aa50d40f84901d9b9cb344cdae6
Alexey Patsovsky <alexey.patsovsky@itechart-group.com>
2017-10-10 11:42:02 +0300

    '10

--- 10.py
+++ 10.py
@@ -4,10 +4,16 @@ while iter < 30:
     string = arr[iter]
     last = 0
     resstr = ''
-    for i in range(len(string)):
-        print(string[i], string[i+1], i)
+    for i in range(len(string)-1):
         if string[i] != string[i+1]:
-            resstr += str(len(string[last:i])) + string[i]
+            substr = string[last:i+1]
+            resstr += str(len(substr)) + string[i]
             last = i+1
-            print(resstr)
+    substr = string[last:]
+    resstr += str(len(substr)) + string[last]
+    print(resstr)
+    arr.append(resstr)
     iter += 1
+
+print(len(arr[30]))
+#5808
